430589628da6363712dee61658fda1e05fd16054
hardwareontrial <hardwareontrial@gmail.com>
2023-02-13 14:04:51 +0700

    bugfix

--- backend/database/seeders/AppUserLoginTableSeeder.php
+++ backend/database/seeders/AppUserLoginTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\API\Auth\AppUserLogin;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -16,15 +17,13 @@ class AppUserLoginTableSeeder extends Seeder
     public function run()
     {
         if(DB::table('app_user_logins')->count() == 0){
-            DB::table('app_user_logins')->insert([
+            AppUserLogin::create([
                 'nik' => 9000901,
                 's_nik' => '901',
                 'email' => 'admin@molindointigas.co.id',
                 'password' => Hash::make('password'),
                 'active' => 1,
                 'admin' => 1,
-                'created_at' => '2022-03-01 07:01:01',
-                'updated_at' => '2022-03-01 07:01:01',
             ],
             [
                 'nik' => '7000701',
@@ -33,9 +32,27 @@ class AppUserLoginTableSeeder extends Seeder
                 'password' => Hash::make('mig123!'),
                 'active' => 1,
                 'admin' => 0,
-                'created_at' => '2022-12-01 17:01:01',
-                'updated_at' => '2022-12-01 17:01:01',
             ]);
+            // DB::table('app_user_logins')->insert([
+            //     'nik' => 9000901,
+            //     's_nik' => '901',
+            //     'email' => 'admin@molindointigas.co.id',
+            //     'password' => Hash::make('password'),
+            //     'active' => 1,
+            //     'admin' => 1,
+            //     'created_at' => '2022-03-01 07:01:01',
+            //     'updated_at' => '2022-03-01 07:01:01',
+            // ],
+            // [
+            //     'nik' => '7000701',
+            //     's_nik' => '701',
+            //     'email' => 'adminokm@migisapp.co.id',
+            //     'password' => Hash::make('mig123!'),
+            //     'active' => 1,
+            //     'admin' => 0,
+            //     'created_at' => '2022-12-01 17:01:01',
+            //     'updated_at' => '2022-12-01 17:01:01',
+            // ]);
         }else{
             echo 'Table not empty';
         }
--- backend/database/seeders/AppUserTableSeeder.php
+++ backend/database/seeders/AppUserTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\API\User\AppUser;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -15,7 +16,7 @@ class AppUserTableSeeder extends Seeder
     public function run()
     {
         if(DB::table('app_users')->count() == 0){
-            DB::table('app_users')->insert([
+            AppUser::create([
                 'nik' => '9000901',
                 'name' => 'ADMINISTRATOR',
                 'dept_id' => null,
@@ -23,8 +24,6 @@ class AppUserTableSeeder extends Seeder
                 'group_id' => null,
                 'avatar' => null,
                 'active' => 1,
-                'created_at' => '2022-03-01 07:01:01',
-                'updated_at' => '2022-03-01 07:01:01',
             ],
             [
                 'nik' => '7000701',
@@ -34,9 +33,29 @@ class AppUserTableSeeder extends Seeder
                 'group_id' => null,
                 'avatar' => null,
                 'active' => 1,
-                'created_at' => '2022-12-01 17:01:01',
-                'updated_at' => '2022-12-01 17:01:01',
             ]);
+            // DB::table('app_users')->insert([
+            //     'nik' => '9000901',
+            //     'name' => 'ADMINISTRATOR',
+            //     'dept_id' => null,
+            //     'position_id' => null,
+            //     'group_id' => null,
+            //     'avatar' => null,
+            //     'active' => 1,
+            //     'created_at' => '2022-03-01 07:01:01',
+            //     'updated_at' => '2022-03-01 07:01:01',
+            // ],
+            // [
+            //     'nik' => '7000701',
+            //     'name' => 'ADMIN OKM',
+            //     'dept_id' => null,
+            //     'position_id' => null,
+            //     'group_id' => null,
+            //     'avatar' => null,
+            //     'active' => 1,
+            //     'created_at' => '2022-12-01 17:01:01',
+            //     'updated_at' => '2022-12-01 17:01:01',
+            // ]);
         }else{
             echo 'Table not empty';
         }
