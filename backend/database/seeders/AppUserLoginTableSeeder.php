e1cbade560ac10c555ad31d7e9c7d4ca49e7ea31
hardwareontrial <hardwareontrial@gmail.com>
2023-02-13 14:13:10 +0700

    bugfix

--- backend/database/seeders/AppUserLoginTableSeeder.php
+++ backend/database/seeders/AppUserLoginTableSeeder.php
@@ -17,42 +17,29 @@ class AppUserLoginTableSeeder extends Seeder
     public function run()
     {
         if(DB::table('app_user_logins')->count() == 0){
-            AppUserLogin::create([
-                'nik' => 9000901,
-                's_nik' => '901',
-                'email' => 'admin@molindointigas.co.id',
-                'password' => Hash::make('password'),
-                'active' => 1,
-                'admin' => 1,
-            ],
-            [
-                'nik' => '7000701',
-                's_nik' => '701',
-                'email' => 'adminokm@migisapp.co.id',
-                'password' => Hash::make('mig123!'),
-                'active' => 1,
-                'admin' => 0,
-            ]);
-            // DB::table('app_user_logins')->insert([
-            //     'nik' => 9000901,
-            //     's_nik' => '901',
-            //     'email' => 'admin@molindointigas.co.id',
-            //     'password' => Hash::make('password'),
-            //     'active' => 1,
-            //     'admin' => 1,
-            //     'created_at' => '2022-03-01 07:01:01',
-            //     'updated_at' => '2022-03-01 07:01:01',
-            // ],
-            // [
-            //     'nik' => '7000701',
-            //     's_nik' => '701',
-            //     'email' => 'adminokm@migisapp.co.id',
-            //     'password' => Hash::make('mig123!'),
-            //     'active' => 1,
-            //     'admin' => 0,
-            //     'created_at' => '2022-12-01 17:01:01',
-            //     'updated_at' => '2022-12-01 17:01:01',
-            // ]);
+            $userslogin = [
+                [
+                    'nik' => 9000901,
+                    's_nik' => '901',
+                    'email' => 'admin@molindointigas.co.id',
+                    'password' => Hash::make('password'),
+                    'active' => 1,
+                    'admin' => 1,
+                    'created_at' => '2022-03-01 07:01:01',
+                    'updated_at' => '2022-03-01 07:01:01',
+                ],
+                [
+                    'nik' => '7000701',
+                    's_nik' => '701',
+                    'email' => 'adminokm@migisapp.co.id',
+                    'password' => Hash::make('mig123!'),
+                    'active' => 1,
+                    'admin' => 0,
+                    'created_at' => '2022-12-01 17:01:01',
+                    'updated_at' => '2022-12-01 17:01:01',
+                ]
+            ];
+            AppUserLogin::insert($userslogin);
         }else{
             echo 'Table not empty';
         }
--- backend/database/seeders/AppUserTableSeeder.php
+++ backend/database/seeders/AppUserTableSeeder.php
@@ -16,46 +16,31 @@ class AppUserTableSeeder extends Seeder
     public function run()
     {
         if(DB::table('app_users')->count() == 0){
-            AppUser::create([
-                'nik' => '9000901',
-                'name' => 'ADMINISTRATOR',
-                'dept_id' => null,
-                'position_id' => null,
-                'group_id' => null,
-                'avatar' => null,
-                'active' => 1,
-            ],
-            [
-                'nik' => '7000701',
-                'name' => 'ADMIN OKM',
-                'dept_id' => null,
-                'position_id' => null,
-                'group_id' => null,
-                'avatar' => null,
-                'active' => 1,
-            ]);
-            // DB::table('app_users')->insert([
-            //     'nik' => '9000901',
-            //     'name' => 'ADMINISTRATOR',
-            //     'dept_id' => null,
-            //     'position_id' => null,
-            //     'group_id' => null,
-            //     'avatar' => null,
-            //     'active' => 1,
-            //     'created_at' => '2022-03-01 07:01:01',
-            //     'updated_at' => '2022-03-01 07:01:01',
-            // ],
-            // [
-            //     'nik' => '7000701',
-            //     'name' => 'ADMIN OKM',
-            //     'dept_id' => null,
-            //     'position_id' => null,
-            //     'group_id' => null,
-            //     'avatar' => null,
-            //     'active' => 1,
-            //     'created_at' => '2022-12-01 17:01:01',
-            //     'updated_at' => '2022-12-01 17:01:01',
-            // ]);
+            $users = [
+                [
+                    'nik' => '9000901',
+                    'name' => 'ADMINISTRATOR',
+                    'dept_id' => null,
+                    'position_id' => null,
+                    'group_id' => null,
+                    'avatar' => null,
+                    'active' => 1,
+                    'created_at' => '2022-03-01 07:01:01',
+                    'updated_at' => '2022-03-01 07:01:01',
+                ],
+                [
+                    'nik' => '7000701',
+                    'name' => 'ADMIN OKM',
+                    'dept_id' => null,
+                    'position_id' => null,
+                    'group_id' => null,
+                    'avatar' => null,
+                    'active' => 1,
+                    'created_at' => '2022-12-01 17:01:01',
+                    'updated_at' => '2022-12-01 17:01:01',
+                ]
+            ];
+            AppUser::insert($users);
         }else{
             echo 'Table not empty';
         }
