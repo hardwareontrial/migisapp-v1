8555d1711026fcb556862bccaab913b953d6d751
hardwareontrial <hardwareontrial@gmail.com>
2023-03-28 16:00:09 +0700

    remove unused function

--- backend/app/Http/Controllers/API/DeliveryNote/AppSuratJalanExportController.php
+++ backend/app/Http/Controllers/API/DeliveryNote/AppSuratJalanExportController.php
@@ -22,25 +22,4 @@ class AppSuratJalanExportController extends Controller
     
     return Excel::download(new ByDefault($creatorid, $createstartdate, $createenddate, $sentstartdate, $sentenddate), $filename);
   }
-
-  public function secondmethod()
-  {
-    $url = 'https://bit.ly/';
-    $urlpath = 'induksi-tamu';
-    $qrname = 'barcode-'.$urlpath.'(2).png';
-    // QrCode::format('png')
-    //   ->gradient(0, 142, 166, 164, 57, 35, 'diagonal')
-    //   ->style('round')
-    //   ->size(500)
-    //   ->merge('/public/image/mig.png', .28)
-    //   ->generate($url.$urlpath, storage_path('app/public/app_inventaris/qrcode/'.$qrname));
-    QrCode::format('png')
-      ->backgroundColor(255, 156, 0)
-      // ->color(244, 231, 11)
-      // ->gradient(244, 231, 11, 164, 57, 35, 'diagonal')  
-      ->style('round')
-      ->size(500)
-      ->merge('/public/image/mig.png', .28)
-      ->generate($url.$urlpath, storage_path('app/public/app_inventaris/qrcode/'.$qrname));
-  }
 }
