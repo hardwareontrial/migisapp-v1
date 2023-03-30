73ed55d608a61463351120de0cfe6651dd35fc8a
hardwareontrial <hardwareontrial@gmail.com>
2023-03-30 14:58:34 +0700

    add admin dashboard

---
+++ frontend/src/component/apps/elearning/ElearningDashboardAdmin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Dashboard Admin
+  </div>
+</template>
+
+<script>
+  export default{
+    beforeCreate() {
+      this.$store.commit('appConfig/UPDATE_LAYOUT_TYPE', 'vertical')
+    }
+  }
+</script>
+
+<style></style>
