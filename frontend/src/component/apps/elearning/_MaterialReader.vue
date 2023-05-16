020776901476d077a1ecac5029f5d4a6618b822c
hardwareontrial <hardwareontrial@gmail.com>
2023-05-16 16:12:16 +0700

    remove card from unused component

--- frontend/src/component/apps/elearning/_MaterialReader.vue
+++ frontend/src/component/apps/elearning/_MaterialReader.vue
@@ -1,31 +1,42 @@
 <template>
   <div>
     <iframe
-      :src="file+`${'#toolbar=0'}`" 
+      id="iframe"
+      :src="file + `${'#toolbar=0'}`"
       width="100%"
       height="600px"
-      allowfullscreen>
+      allowfullscreen
+      frameborder="0"
+    >
     </iframe>
   </div>
 </template>
 
 <script>
-import {
-  BCard,
-} from 'bootstrap-vue'
-
 export default {
-  components: {
-    BCard,
-  },
   props: {
     file: {
       type: String,
-    }
+    },
   },
-}
+  mounted() {
+    var data = document.getElementById("iframe");
+    var dataDoc = data.contentDocument || data.contentWindow.document;
+    if (!dataDoc) {
+      console.log("Not found in DOM");
+    }
+    // var iframeContent = dataDoc.getElementById("toolbarViewer");
+    // var iframeContent = dataDoc.querySelectorAll('toolbarViewer')
+    // console.log(dataDoc);
+  },
+  watch: {
+    file: {
+      handler(newValue) {
+        console.log(newValue);
+      },
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
